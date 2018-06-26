7730b9a78a9351e0c2343bdbfa88007b582212e2
eltn <https://github.com/mkEldor/blackstone>
2018-06-26 22:38:03 +0600

    Initial Commit

---
+++ HW-2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HW-2
+//
+//  Created by Eldor Makkambayev on 26.06.2018.
+//  Copyright © 2018 Eldor Makkambayev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
